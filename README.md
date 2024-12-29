7653f3a7730dec7f315ecac1d4b140ddb4ea6aaf
aBDevs <devops.abis@gmail.com>
2024-12-29 22:23:14 +0100

    modification de module, consommation API server.py, ajout de hostname depuis le playbook, gestion password & username

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 ## Projet : Gestion IPAM et DNS avec Ansible
 
 ### 1. Introduction
-Notre objectif est de créer des modules Ansible qui consomment des APIs distantes pour gérer les adresses IP et les enregistrements DNS. Ce projet se compose de trois modules principaux : ipam_free_ip, dns_register, et manager_access.
+Notre objectif est de créer des modules Ansible qui consomment des APIs distantes pour gérer les adresses IP et les enregistrements DNS. Ce projet se compose de trois modules principaux : `ipam_free_ip`, `dns_register`, et `manager_access`.
+
 
 ### 2. Module IPAM : ipam_free_ip
-Le module ipam_free_ip sélectionne une adresse IP disponible de manière aléatoire à partir d'un fichier JSON. 
 
-- Voici un exemple de fichier JSON pour ce module :
+Le module `ipam_free_ip` sélectionne une adresse IP disponible de manière aléatoire à partir d'un fichier JSON.
+
+- **Exemple de fichier JSON pour ce module :**
 
 ```json
 {
@@ -15,9 +17,8 @@ Le module ipam_free_ip sélectionne une adresse IP disponible de manière aléat
     {"ip": "1.1.1.2", "hostname": ""}
   ]
 }
-```
 
-### 3. Module DNS : dns_register
+### 3. Module DNS : 
 Le module dns_register teste la validation de l'adresse IP sélectionnée par le module IPAM. Ensuite, il vérifie si l'IP n'est pas déjà utilisée, puis l'ajoute dans un fichier dns.json. 
 
 - Voici un exemple de fichier JSON pour ce module :
@@ -46,6 +47,16 @@ Le module manager_access génère les noms d'utilisateur et mots de passe pour c
 
 ### 5. Exécution de playbook et de server Flask
 
+- Prérequis
+```sh
+  - Python 3.9
+  - Ansible
+  - Flask
+```
+```sh
+python -m venv venv
+source venv/bin/activate
+```
 ```sh
 python3  server.py
 ```
--- server.py
+++ server.py
@@ -1,23 +1,138 @@
 from flask import Flask, jsonify, request
+import json
+import random
+import string
 
 app = Flask(__name__)
+
+ipam_file="ip_list.json"
+access_file = "access_list.json"
 
 @app.route('/ipam/free_ip', methods=['GET'])
 def get_free_ip():
     # Simuler une réponse avec une IP libre
-    response = {
-        "message": "It's Ok"
-    }
-    
-    return jsonify(response), 200
+    try:
+        with open(ipam_file, 'r') as file:
+            ipam_data = json.load(file)
+        ips_libre = [item for item in ipam_data['ipam'] if item['hostname'] == ""]
 
-@app.route('/dns/register_dns', methods=['POST'])
-def register_dns():
+        if not ips_libre:
+            response = {
+            "free_ip": ""
+             }
+            
+            return jsonify(response), 201
+
+        chosen_ip_info = random.choice(ips_libre)
+        response = {
+            "free_ip": chosen_ip_info['ip']
+             }
+
+        return jsonify(response), 201
+    
+        # crear une boucle for pour verifier si IP est libre
+        #  et d'ajouter à l'IP choisi un hostname
+
+    except Exception as e:
+        response = {"message": e }
+        return jsonify(response), 500
+    
+@app.route('/ipam/reserve_ip', methods=['POST'])
+def reservation_ip():
     data = request.json
     ip = data.get('ip')
     hostname = data.get('hostname')
-    # Ajoutez ici la logique pour enregistrer l'entrée DNS
-    return jsonify({"message": "DNS entry registered successfully"}), 200
+
+    try:
+        with open(ipam_file, 'r') as file:
+            ipam_data = json.load(file)
+    
+        ips_reserve = [item for item in ipam_data['ipam'] if item['ip'] == ip]
+        
+        if not ips_reserve:
+            response = { "message": "IP not exist"}
+            return jsonify(response), 500
+        
+        if len(ips_reserve)>1:
+            response = { "message": "IP exite plusieurs fois  "}
+            return jsonify(response), 500
+        
+        if ips_reserve[0]['hostname'] != "" :
+            if ips_reserve[0]['hostname'] == str(hostname[0]):
+                response = { "message": "IP deja enregistré  avec le bon hostname" }
+                return jsonify(response), 200
+        
+            if ips_reserve[0]['hostname'] != str(hostname[0]):
+                response = { "message": "IP est déja reservé par un autre hostname " }
+                return jsonify(response), 400
+        
+
+        for item in ipam_data['ipam']:
+            if item['ip'] == ip:
+                item['hostname'] = str(hostname[0])
+      
+        # Save the updated IPAM data back to the file
+        with open(ipam_file, 'w') as file:
+            json.dump(ipam_data, file)
+
+        response = { "message": "IP est reservé avec un bon hostname " }
+        return jsonify(response), 200
+
+    except Exception as e:
+        response = { "message": e }
+        return jsonify(response), 500    
+
+
+@app.route('/manage_access', methods=['POST'])
+def manage_access():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        response = {"message": "Username and password are required"}
+        return jsonify(response), 400
+
+    try:
+        # Lire le fichier ip_list.json
+        with open(ipam_file, 'r') as f:
+            ipam_data = json.load(f)
+
+        # Lire le fichier access_file.json
+        try:
+            with open(access_file, 'r') as f:
+                access_data = json.load(f)
+        except FileNotFoundError:
+            access_data = {"manager_access": []}
+
+        # Récupérer les hostnames non vides et les enregistrer dans access_file.json
+        for item in ipam_data['ipam']:
+            if item['hostname'] != "":
+                # Vérifier si le hostname existe déjà dans access_data["manager_access"]
+                host_exists = False
+                for tab in access_data["manager_access"]:
+                    if tab['hostname'] == item['hostname']:
+                        host_exists = True
+                        break
+
+                if not host_exists:
+                    access_data["manager_access"].append({
+                        "ip": item['ip'],
+                        "hostname": item['hostname'],
+                        "username": username,
+                        "password": password
+                    })
+
+        # Sauvegarder les données mises à jour dans le fichier access_file.json
+        with open(access_file, 'w') as file:
+            json.dump(access_data, file, indent=4)
+
+        response = {"message": "Access managed successfully"}
+        return jsonify(response), 200
+
+    except Exception as e:
+        response = {"message": str(e)}
+        return jsonify(response), 500
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000)
